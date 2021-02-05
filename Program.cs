1d1ed2a5f92b016009a0354ec430c2afa6475320
nhrstv <76903916+nhrstv@users.noreply.github.com>
2021-02-05 20:26:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateModifierProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+       
+            Console.WriteLine(Math.Abs(DateModifier.GetDiffBetweenDatesInDays(firstDate, secondDate)));
+            
+        }
+
+       
+
+
+    }
+}
